f7774f7841b765d14e078a9f3fd99fc3f60f6cdc
Gabriel <lukaodogame@gmail.com>
2024-04-01 21:55:29 -0300

    fist commit

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/XxSusuxX.png" />
+    <Titulo>Gabriel Suenaga</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
+      laudantium dolor explicabo laborum ipsam, velit eum nisi vel sapiente
+      ipsum, doloribus, sunt mollitia ea impedit! Doloribus doloremque
+      dignissimos tempora amet!
+    </p>
+  </section>
+)
+
+export default Sobre
